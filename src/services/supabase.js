d26c917a7aba6aa1d367e0363768bd90a7d722d5
nastaj <G00424689@atu.ie>
2023-09-25 15:18:59 +0200

    supabase integration

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://hzgvkffmhncxxihhclie.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6Z3ZrZmZtaG5jeHhpaGhjbGllIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU2MzUyODYsImV4cCI6MjAxMTIxMTI4Nn0.71XOxqBu-4KaGtJep5siEo0R3xAokcHMEFyq1P2S394";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
