28db34cea2e5b7aa617e6e2ee1849bf566f97c57
nastaj <G00424689@atu.ie>
2023-09-27 23:54:14 +0200

    modal

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,10 +1,10 @@
-import { updateSetting } from "../../services/apiSettings";
+import { useSettings } from "./useSettings";
+import { useUpdateSetting } from "./useUpdateSetting";
+
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import Spinner from "../../ui/Spinner";
-import { useSettings } from "./useSettings";
-import { useUpdateSetting } from "./useUpdateSetting";
 
 function UpdateSettingsForm() {
   const {
