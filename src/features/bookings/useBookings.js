9bcd05ca75f03aa90286f2ce0f2861fd70376731
nastaj <G00424689@atu.ie>
2023-09-29 15:33:44 +0200

    filtering

---
+++ src/features/bookings/useBookings.js
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
+
+export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
+
+  const {
+    data: bookings,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
+  });
+
+  return { bookings, isLoading, error };
+}
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -5,13 +5,17 @@ import CabinRow from "./CabinRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
-  const { isLoading, cabins } = useCabins();
+  const { isLoading, cabins = [] } = useCabins();
   const [searchParams] = useSearchParams();
+
+  if (!cabins.length) return <Empty resourceName="cabins" />;
 
   if (isLoading) return <Spinner />;
 
+  // 1) FILTER
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
@@ -20,6 +24,28 @@ function CabinTable() {
     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // 2) SORT
+  const sortBy = searchParams.get("sortBy") || "name-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+
+  function compareText(a, b) {
+    if (a[field].toLowerCase() < b[field].toLowerCase()) {
+      return -1 * modifier;
+    }
+    if (a[field].toLowerCase() > b[field].toLowerCase()) {
+      return 1 * modifier;
+    }
+    return 0;
+  }
+  function compareNumbers(a, b) {
+    return (a[field] - b[field]) * modifier;
+  }
+  const sortedCabins =
+    typeof cabins[0][field] === "number"
+      ? filteredCabins.sort(compareNumbers)
+      : filteredCabins.sort(compareText);
 
   return (
     <Menus>
@@ -30,11 +56,10 @@ function CabinTable() {
           <div>Capacity</div>
           <div>Price</div>
           <div>Discount</div>
-          <div>xxx</div>
         </Table.Header>
 
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
