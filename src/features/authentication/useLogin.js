7da2fedfd87ad631dd409d07ac8e89e2fadb8079
nastaj <G00424689@atu.ie>
2023-10-01 22:46:18 +0200

    logout

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -11,8 +11,8 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
 
     onSuccess: (user) => {
-      queryClient.setQueriesData(["user"], user);
-      navigate("/dashboard");
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/dashboard", { replace: true });
     },
     onError: () => {
       toast.error("Provided email or password are incorrect");
